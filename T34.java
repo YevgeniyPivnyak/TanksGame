ad5cf0ac4b6e70a9a14fda9ae9dd8bce6e94ca0b
YevgeniyPivnyak <dneprojek@gmail.com>
2016-04-30 21:36:45 +0300

    added T34 defender

--- T34.java
+++ T34.java
@@ -2,10 +2,14 @@ import java.awt.*;
 
 public class T34 extends AbstractTank {
 
+    public T34 (ActionField af, BattleField bf) {
+        this(af, bf, 64, 512, Direction.UP);
+    }
+
     public T34(ActionField actionField, BattleField battleField, int x, int y, Direction direction) {
         super(actionField, battleField, x, y, direction);
         speed = 10;
-        tankColor = new Color(255, 0, 0);
-        towerColor = new Color(0, 255, 0);
+//        tankColor = new Color(255, 0, 0);
+//        towerColor = new Color(0, 255, 0);
     }
 }
